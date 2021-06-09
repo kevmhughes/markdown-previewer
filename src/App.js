8b3f100159cb36a0e26d1f39e76e43e25088f006
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-06-10 01:23:16 +0200

    Add styling to preview element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { sampleText } from './sampleText';
 import marked from 'marked';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+        text: sampleText
     }
   }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({text}
+      )} else {
+        this.setState({text: sampleText})
+      }
+    }
+
+  componentDidUpdate() {
+    const { text } = this.state 
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = (event) =>  {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = (text) => {
+    const __html = marked(text, {sanitize: true, breaks: true, gfm: true, highlight: null, })
+    return { __html }
+  }
+
+
   render () {
     return (
         <div className="container">
           <div className="row">
             <div className="col-sm-6">
-          <textarea id="editor" className="form-control" rows="35" />
-
-            <div id="preview">
-            <p>Hello</p>
+              <textarea 
+                id="editor" 
+                onChange={this.handleChange}
+                value= {this.state.text} 
+                className="form-control" 
+                rows="35" />
             </div>
+            <div className="col-sm-6">
+              <div id="preview" dangerouslySetInnerHTML={this.renderText(this.state.text)} ></div>
             </div>
           </div>
         </div>
